b02c4b95f189cc769d868cc8a2f9d77cf4c35a3b
jcastro@taycan <jcastro@taycan>
2022-07-15 18:52:52 -0300

    /bin organizado

--- bin/install.sh
+++
@@ -1,7 +0,0 @@
-
-
-#sudo virt-builder -v --format qcow2  ubuntu-18.04 -o /var/lib/libvirt/images/vm1.qcow2 --size 30G --hostname vm1 --root-password password:password --run /home/jcastro/devops/kvm/install.sh    --ssh-inject devops:file:/home/jcastro/.ssh/id_rsa.pub 
-
-
-
-
